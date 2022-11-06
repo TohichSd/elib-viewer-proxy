60ebcbcf0817386301ed746fb26829336ca5fbe0
TohichSd <anton.sur17@gmail.com>
2022-11-07 00:54:40 +0300

    Stable V1.0

--- index.js
+++ index.js
@@ -1,63 +1,22 @@
-import * as http from 'http'
-import * as https from 'https'
-import fetch from 'node-fetch'
-import * as utf8 from 'utf8'
+import express from 'express'
+import {createProxyMiddleware} from 'http-proxy-middleware'
 
-const agent = new http.Agent()
+const app = express()
 
-const server = http.createServer(async (req, res) => {
-  let body = ''
-  let url
-  try{
-    url = new URL(req.url.slice(1, req.url.length))
-  }
-  catch (e) {
-    res.end()
-    return
-  }
-  req.on('data', chunk => {
-    body += chunk.toString()
-  })
-  req.on('end', async () => {
-    try {
-      const proxy_res = await fetch(url.href, {
-        method: req.method,
-        body: req.method.toUpperCase() === 'GET' ? undefined : body,
-        headers: {
-          Hostname: url.host,
-          'Content-Type': req.headers['content-type']
-        },
-        redirect: 'manual',
-        agent
-      })
-      res.writeHead(200)
-      const rawHeaders = {}
-      Object.keys(proxy_res.headers.raw()).forEach(k => {
-        rawHeaders[k] = proxy_res.headers.raw()[k][0]
-      })
-      res.end(JSON.stringify({
-        status: proxy_res.status,
-        statusText: proxy_res.statusText,
-        headers: rawHeaders,
-        url: proxy_res.url,
-        text: await proxy_res.text(),
-        setCookie: proxy_res.headers.get('set-cookie').split(',')
-      }))
-    } catch (e) {
-      console.error(e)
-    }
+const onProxyRes = (proxyRes, req) => {
+  proxyRes.headers['Access-Control-Allow-Origin'] = req.headers['origin'] || req.headers['referer']
+  proxyRes.headers['Access-Control-Allow-Credentials'] = 'true'
+  proxyRes.headers['Access-Control-Allow-Headers'] = '*'
+  proxyRes.headers['Access-Control-Expose-Headers'] = 'true'
+}
 
-    const proxy_res1 = await fetch(url.href+'?cookieVerify=', {
-      method: req.method,
-      body: req.method.toUpperCase() === 'GET' ? undefined : body,
-      headers: {
-        Hostname: url.host,
-        'Content-Type': req.headers['content-type']
-      },
-      redirect: 'manual',
-      agent
-    })
-  })
-})
+const options = {
+  target: 'http://elib.mpei.ru',
+  changeOrigin: true,
+  onProxyRes,
+  followRedirects: false
+}
 
-server.listen(3000)
+app.use('/', createProxyMiddleware(options))
+
+app.listen(3000)
