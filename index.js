400b69e08a8393cbc5a6011aa05aa70bd510ab32
TohichSd <anton.sur17@gmail.com>
2022-11-14 00:13:00 +0300

    Stable V1.3

--- index.js
+++ index.js
@@ -6,11 +6,17 @@ import { readFileSync } from 'fs'
 
 const app = express()
 
+const PORT = 3000
+
 const onProxyRes = (proxyRes, req) => {
+  if (proxyRes.headers['set-cookie'])
+    console.log(proxyRes.headers['set-cookie'].map(header => header + '; SameSite=None'))
   proxyRes.headers['Access-Control-Allow-Origin'] = req.headers['origin'] || req.headers['referer']
   proxyRes.headers['Access-Control-Allow-Credentials'] = 'true'
   proxyRes.headers['Access-Control-Allow-Headers'] = '*'
   proxyRes.headers['Access-Control-Expose-Headers'] = 'true'
+  if (proxyRes.headers['location'])
+    proxyRes.headers['location'] = proxyRes.headers['location'].replace('http://elib.mpei.ru', '')
 }
 
 const options = {
@@ -31,5 +37,5 @@ if (env.SSL_CERT_PATH && env.SSL_KEY_PATH) {
       },
       app
     )
-    .listen(3000)
-} else app.listen(3000)
+    .listen(PORT)
+} else app.listen(PORT)
