dadf6e41442db393062c6597fb55c237371cb33d
TohichSd <anton.sur17@gmail.com>
2022-11-20 22:16:04 +0300

    Stable V1.4 - улучшено взаимодействие с https

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ import { env } from 'process'
 import * as https from 'https'
 import { readFileSync } from 'fs'
 
+const PORT = 3000
+const isSSL = env.SSL_CERT_PATH && env.SSL_KEY_PATH
+
 const app = express()
 
-const PORT = 3000
 
 const onProxyRes = (proxyRes, req) => {
   if (proxyRes.headers['set-cookie'])
-    proxyRes.headers['set-cookie'] = proxyRes.headers['set-cookie'].map(header => header + '; SameSite=None')
+    proxyRes.headers['set-cookie'] = proxyRes.headers['set-cookie'].map(header => header + '; SameSite=None; Secure')
   proxyRes.headers['Access-Control-Allow-Origin'] = req.headers['origin'] || req.headers['referer']
   proxyRes.headers['Access-Control-Allow-Credentials'] = 'true'
   proxyRes.headers['Access-Control-Allow-Headers'] = '*'
@@ -19,22 +21,27 @@ const onProxyRes = (proxyRes, req) => {
     proxyRes.headers['location'] = proxyRes.headers['location'].replace('http://elib.mpei.ru', '')
 }
 
+let ssl = undefined
+if (isSSL)
+  ssl = {
+    key: readFileSync(env.SSL_KEY_PATH).toString(),
+    cert: readFileSync(env.SSL_CERT_PATH).toString(),
+  }
+
 const options = {
   target: 'http://elib.mpei.ru',
   changeOrigin: true,
   onProxyRes,
-  followRedirects: false
+  followRedirects: false,
+  ssl
 }
 
 app.use('/', createProxyMiddleware(options))
 
-if (env.SSL_CERT_PATH && env.SSL_KEY_PATH) {
+if (isSSL) {
   https
     .createServer(
-      {
-        key: readFileSync(env.SSL_KEY_PATH).toString(),
-        cert: readFileSync(env.SSL_CERT_PATH).toString(),
-      },
+      ssl,
       app
     )
     .listen(PORT)
